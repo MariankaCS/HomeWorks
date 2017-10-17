26840c6af4210c0f0528cc10647b6e398c69c87d
mariana tsiupka <mariana.tsiupka@gmail.com>
2017-10-17 15:29:43 +0300

    Homework #5 part #1 and part #2

---
+++ Homework5/Builder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5
+{
+    public class Builder : IDeveloper, IComparable<Builder>
+    {
+        private string tool;
+        public string Tool
+        {
+            get {return tool;}
+            set {tool = value;}
+        }
+
+        public int CompareTo(Builder obj)
+        {
+            return Tool.CompareTo(obj.Tool);
+        }
+
+
+        public string Create()
+        {
+            return $"I am a builder and I build with {Tool}";
+        }
+
+        public string Destroy()
+        {
+            return $"I can destroy with {Tool}";
+        }
+    }
+}
---
+++ Homework5_Part2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework5_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2.Create Console Application project in VS.
+            //In the Main() method declare Dictionary<uint, string>.
+            //Add to Dictionary from Console seven pairs(ID, Name) of some persons.
+            //Ask user to enter ID, then find and write corresponding Name 
+            //from your Dictionary.If you can't 
+            //find this ID - say about it to user.
+            Dictionary<uint, string> myDictionary = new Dictionary<uint, string>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter ID");
+                uint value = uint.Parse(Console.ReadLine());
+                Console.WriteLine("Eneter name");
+                string name = Console.ReadLine();
+                myDictionary.Add(value, name);
+            }
+
+            FindNameINDictionary(myDictionary);
+            Console.ReadLine();
+
+        }
+
+        static void FindNameINDictionary(Dictionary<uint, string> myDictionary)
+        {
+            Console.WriteLine("Enter ID of a person you want to retrieve");
+            uint choice = uint.Parse(Console.ReadLine());
+
+            string result = myDictionary.ContainsKey(choice) ? myDictionary[choice] : "There is no such ID";
+            Console.WriteLine(result);
+          
+        }
+    }
+}
+        
+  
+
